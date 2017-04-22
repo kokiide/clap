39e59b19ff1a9639d5611c2117248e45e0f7bc76
Koki.Ide <s15087ki@sfc.keio.ac.jp>
2017-04-22 17:50:58 +0900

    1st

--- Clap/ViewController.swift
+++ Clap/ViewController.swift
@@ -7,17 +7,42 @@
 //
 
 import UIKit
+import AVFoundation
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource
+{
+    @IBOutlet var clapPickerView: UIPickerView!
+    
+        var audioplayer : AVAudioPlayer!
+    var soundCount : Int = 0
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        let soundFilePath = Bundle.main.path(forResource: "clap", ofType: "wav")!
+        let fileURL = URL(fileURLWithPath: soundFilePath)
+        
+        //AVAudioPlayerのインスタンス化
+        do{
+            audioplayer = try AVAudioPlayer(contentsOf: fileURL)
+        } catch {
+            print("音楽が読み込めませんでした")
+        }
+        clapPickerView.delegate = self
+        clapPickerView.dataSource = self
+    }
+    
+    @IBAction func playButton(){
+        audioplayer.play()
     }
 
+    
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+        
+        
     }
 
 
